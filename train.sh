6bb848b74e11ebe2fa8987e146023f1b866ce283
Pham Van Hung <phamhung1010.it@gmail.com>
2024-07-05 10:26:17 +0700

    Fix bug tranining single task classification

--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
 
+
 # # # Training multitask 
 # python train_multitask.py \
 #     --batch_size 1024 \
@@ -21,8 +22,27 @@
 
 
 
- python train_kan_multitask_orthogonal.py \
-    --batch_size 1024 \
+#  python train_kan_multitask_orthogonal.py \
+#     --batch_size 1024 \
+#     --epochs 600 \
+#     --input_dim 3 \
+#     --n_hidden_1 128 \
+#     --n_hidden_2 64 \
+#     --n_classes 12 \
+#     --p_dropout 0.25 \
+#     --learning_rate 0.001 \
+#     --seed 42 \
+#     --log_steps 5 \
+#     --w_regression 0.1 \
+#     --w_classify 0.5 \
+#     --w_grad 0.5 \
+#     --data_path "data/multitask_cls12_regr.npz" \
+#     --output_dir "models/multitask_LSTM_orthogonal" \
+#     --project_name "MTL - Experiments" \
+#     --experiment_name "mtl-LSTM-128-64-orthogonal-reg0.1-cls0.5-grad0.5" \
+
+python train_classify.py \
+    --batch_size 512 \
     --epochs 600 \
     --input_dim 3 \
     --n_hidden_1 128 \
@@ -32,10 +52,7 @@
     --learning_rate 0.001 \
     --seed 42 \
     --log_steps 5 \
-    --w_regression 0.1 \
-    --w_classify 0.5 \
-    --w_grad 0.5 \
     --data_path "data/multitask_cls12_regr.npz" \
-    --output_dir "models/multitask_LSTM_orthogonal" \
-    --project_name "MTL - Experiments" \
-    --experiment_name "mtl-LSTM-128-64-orthogonal-reg0.1-cls0.5-grad0.5" \
+    --output_dir "models/single_task_classify" \
+    --project_name "experiments-research" \
+    --experiment_name "single-task-LSTM-128-64"
--- train_classify.py
+++ train_classify.py
@@ -7,7 +7,7 @@ from dataset import get_data_mtl
 from dataset import ClassifyDataset
 from trainer.classify_trainer import ClassifyTrainer
 from net import (
-    ClassifyRNN,
+    ClassifyLSTM,
     cls_metric,
     cls_loss_fn
 )
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     # Initialize the PyTorch model
-    model = ClassifyRNN(
+    model = ClassifyLSTM(
         input_size=args.input_dim,
         hidden_size_1=args.n_hidden_1,
         hidden_size_2=args.n_hidden_2,
