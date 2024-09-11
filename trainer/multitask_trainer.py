4009255b30f5e826b7a6b5828fc72b21a874434a
Pham Van Hung <phamhung1010.it@gmail.com>
2024-09-11 22:30:28 +0700

    Test debug

--- trainer/multitask_trainer.py
+++ trainer/multitask_trainer.py
@@ -330,7 +330,7 @@ class MultitaskTrainer(BaseTrainer):
             
             loss = self.w_reg * reg_loss + self.w_cls * cls_loss
             print(f"""\
-Loss reg: {self.w_reg * reg_loss} - Loss cls: {self.w_cls * cls_loss} - Loss grad: {self.w_grad * grad_loss} - Agg loss: {loss}\
+Loss reg: {self.w_reg * reg_loss} - Loss cls: {self.w_cls * cls_loss} - Agg loss: {loss}\
 """)
 
             optimizer.zero_grad()
